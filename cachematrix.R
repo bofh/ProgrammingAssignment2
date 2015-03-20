04aaca5e978dac5f1b4e76e432e29169602fa1dc
bofh <bofh@users.noreply.github.com>
2015-03-20 23:19:29 +0300

    cacheSolve implemented
    NOTE: assuming that the matrix supplied is always invertible

--- cachematrix.R
+++ cachematrix.R
@@ -2,28 +2,46 @@
 ## 
 ## Example usage:
 ##  x <- makeCacheMatrix()
-##  m <- matrix(c(2, 4, 3, 1, 5, 7), nrow=3, ncol=2)
+##  m <- matrix(c(2, 4, 3, 1), nrow=2, ncol=2)
 ##  x$set(m)
 ##  x$get()
 
 ## Constructor for the matrix with cached inverse function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ci <- NULL
 
   set <- function(y) {
     x <<- y
   }
 
-  get <- function() {
+  get <- function() { 
     x
   }
 
-  list(set = set, get = get)
+  setinverse <- function(inverse) {
+    ci <<- inverse
+  }
+
+  getinverse <<- function() {
+    ci
+  }
+
+  list(set = set, get = get,
+       setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## returns the inverse of the matrix, result is cached after the first run
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ci <- x$getinverse()
+  if (!is.null(ci)) {
+    message("returning inverse matrix from cache")
+    return(ci)
+  }
+  m <- x$get()
+  ci <- solve(m, ...)
+  x$setinverse(ci)
+  ci
 }
